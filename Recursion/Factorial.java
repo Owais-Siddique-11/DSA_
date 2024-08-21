32a09e9f999310d41aa5bccba30dd54a499beec7
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2024-08-21 20:21:28 +0530

    commit

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,3 +1,14 @@
+/*
+ Q)Finding factorial of N numbers 
+ */
+
 public class Factorial{
-    
+    static int fact(int n){
+        if(n==1) return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fact(n));   
+    }
 }
